59fa1f96240c7f83ad79f64856fb7a7dfa25faeb
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-10-31 20:05:53 +0000

    Added Ad support, gradle scripts update.

--- app/src/main/java/com/darodev/thuglifephotoeditor/EditorActivity.java
+++ app/src/main/java/com/darodev/thuglifephotoeditor/EditorActivity.java
@@ -1,13 +1,54 @@
 package com.darodev.thuglifephotoeditor;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class EditorActivity extends AppCompatActivity {
+    private AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        //prepareAds();
+    }
+
+    private void prepareAds(){
+        //LinearLayout add_holder = (LinearLayout) findViewById(R.id.layout_ad);
+        //add_holder.addView(getAdView());
+    }
+
+    private AdView getAdView(){
+        if(adView == null){
+            MobileAds.initialize(getApplicationContext(), getResources().getString(R.string.app_id));
+
+            RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            adParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+            adView = new AdView(getApplicationContext());
+            adView.setAdSize(AdSize.SMART_BANNER);
+            adView.setAdUnitId(getResources().getString(R.string.add_unit_id));
+            adView.setBackgroundColor(Color.TRANSPARENT);
+            adView.setLayoutParams(adParams);
+
+            // Test Ads
+            // AdRequest adRequest = new AdRequest.Builder().addTestDevice(getResources().getString(R.string.test_device_id)).build();
+            AdRequest adRequest = new AdRequest.Builder().build();
+
+            adView.loadAd(adRequest);
+        }
+
+        return adView;
     }
 }
